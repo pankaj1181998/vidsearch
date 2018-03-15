b71a7edc7b966fee59bf811ce207f4dfc816562b
Pankaj Sharma <sharmapankaj090@gmail.com>
2018-03-16 02:26:15 +0530

    edits

--- app/controllers/apiyoutube_controller.rb
+++ app/controllers/apiyoutube_controller.rb
@@ -6,31 +6,38 @@ require 'net/http'
 
 class ApiyoutubeController < ApplicationController
 	
+	respond_to :html, :js
+
     def search
 			  
     end
 
  	def ldetails
-		
  	 	@link = params[:chk_link]
 	
 		uri = URI("https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails&id="+@link+"&key=AIzaSyBOaeBxhyOrUz4zt2euYSTiMVoM9sSE4QI")
 		Net::HTTP.get(uri) # => String
-		res = JSON.parse(Net::HTTP.get(uri))
+		@res = JSON.parse(Net::HTTP.get(uri))
 		
 	 	p res
-	 	if !res["items"].empty?
-	 		
-		 	@title = res["items"][0]["snippet"]["title"]
-		 	@desc = res["items"][0]["snippet"]["description"]
-	 		@img = res["items"][0]["snippet"]["thumbnails"]["medium"]
-	 		render partial: 'app/views/apiyoutube/_result.html.erb'	
-	 	else
-		 	print "wrong id	"
-		 	render plain: "CHECK LINK_ID"
-		end
 
-		
+	respond_to do |format|
+    	format.js { render :file => "/path/to/save.js.erb" }
+	end
+
+	
+	 # 	if !res["items"].empty?
+	 # 		@resp = res
+		#  	@title = res["items"][0]["snippet"]["title"]
+		#  	@desc = res["items"][0]["snippet"]["description"]
+	 # 		@img = res["items"][0]["snippet"]["thumbnails"]["medium"]
+			
+	 # 	else
+	 # 		print "wrong id	"
+		 	
+		# end
+
+
 	end	
 
 end

7def2e70499ed405bbb6afb3fb366a5e08351a8b
Pankaj Sharma <sharmapankaj090@gmail.com>
2018-03-20 02:57:45 +0530

    added ajax edits

--- app/controllers/apiyoutube_controller.rb
+++ app/controllers/apiyoutube_controller.rb
@@ -6,37 +6,34 @@ require 'net/http'
 
 class ApiyoutubeController < ApplicationController
 	
-	respond_to :html, :js
 
     def search
 			  
     end
 
  	def ldetails
+ 	 	@title=1
+ 	 	@desc=222
+
  	 	@link = params[:chk_link]
-	
 		uri = URI("https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails&id="+@link+"&key=AIzaSyBOaeBxhyOrUz4zt2euYSTiMVoM9sSE4QI")
 		Net::HTTP.get(uri) # => String
 		@res = JSON.parse(Net::HTTP.get(uri))
 		
-	 	p res
+	 	
 
-	respond_to do |format|
-    	format.js { render :file => "/path/to/save.js.erb" }
-	end
 
-	
-	 # 	if !res["items"].empty?
-	 # 		@resp = res
-		#  	@title = res["items"][0]["snippet"]["title"]
-		#  	@desc = res["items"][0]["snippet"]["description"]
-	 # 		@img = res["items"][0]["snippet"]["thumbnails"]["medium"]
+	 	@title = @res["items"][0]["snippet"]["title"]
+	 	@desc = @res["items"][0]["snippet"]["description"]
+ 		@img = @res["items"][0]["snippet"]["thumbnails"]["medium"]
 			
-	 # 	else
-	 # 		print "wrong id	"
-		 	
-		# end
+		respond_to do |format|
 
+        format.html { } # index.html.erb
+        format.json { render json: @res  }
+        format.js
+	   		end
+	  
 
 	end	
 

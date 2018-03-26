3f4b65dbede58eb58c89d2575bbb4db5a47de600
Pankaj Sharma <sharmapankaj090@gmail.com>
2018-03-26 22:37:20 +0530

    deploy fic

--- config/application.rb
+++ config/application.rb
@@ -9,9 +9,10 @@ Bundler.require(*Rails.groups)
 module Vidsearch
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
-    config.assets.initialize_on_precompile = true
-    config.serve_static_assets = true
+   
+   		 config.load_defaults 5.1
+    	 config.assets.initialize_on_precompile = false
+         config.serve_static_assets = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
